0941aa67b7fd2fff7f51f2af160da8a0f55a4dfb
TapishBalodi <tapishbalodi7@gmail.com>
2024-03-24 05:19:04 +0530

    save

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 venv/bin/activate
-echo $PYTHONPATH
 # Install project dependencies
-pip install -r requirements.txt
+# pip install -r requirements.txt
 
-# Build static files (if applicable)
-python manage.py collectstatic --noinput
+# # Build static files (if applicable)
+# python manage.py collectstatic --noinput
 
-# Perform migrations (if applicable)
-python manage.py migrate
+# # Perform migrations (if applicable)
+# python manage.py migrate
 
 python manage.py runserver
