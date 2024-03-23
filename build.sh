657a8350d4f0e3605a0d731a8413c9db8f14b656
TapishBalodi <tapishbalodi7@gmail.com>
2024-03-24 05:15:27 +0530

    build

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
+venv/bin/activate
 
+# Install project dependencies
 pip install -r requirements.txt
 
-# Run Django migrations
+# Deactivate the virtual environment
+deactivate
+
+# Build static files (if applicable)
+python manage.py collectstatic --noinput
+
+# Perform migrations (if applicable)
 python manage.py migrate
 
 python manage.py runserver
