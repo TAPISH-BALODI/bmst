faebdf3b565a0ba5b1cf49516a169beabca6997d
TapishBalodi <tapishbalodi7@gmail.com>
2024-03-24 04:24:05 +0530

    push

--- manage.py
+++ manage.py
@@ -15,8 +15,7 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from exc
-    execute from command_line(sys.argv)
+        ) from execute from command_line(sys.argv)
 
 
 if __name__ == '__main__':
