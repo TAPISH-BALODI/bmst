105dc0ef0596320b070b9a685d8d267e9c83d1d6
TapishBalodi <tapishbalodi7@gmail.com>
2024-03-24 15:18:05 +0530

    push

--- manage.py
+++ manage.py
@@ -11,7 +11,7 @@ def main():
     # Add the path to your Django project
     current_path = os.path.dirname(os.path.abspath(__file__))
     sys.path.append(os.path.join(current_path, 'bmsBackend'))
-    os.environ['PYTHONPATH'] = '/'
+    os.environ['PYTHONPATH'] = 'venv/Scripts/activate'
 
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bmsBackend.settings')
     print("-->>>> ENV",os.environ)
