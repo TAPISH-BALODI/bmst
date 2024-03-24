57cdf20b3d9bc8360c7732417cfdfbee3ed9af62
TapishBalodi <tapishbalodi7@gmail.com>
2024-03-24 10:35:58 +0530

    push

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ pip install -r requirements.txt
 # python manage.py collectstatic --noinput
 
 # # Perform migrations (if applicable)
+source venv/bin/activate
 python manage.py migrate
 
-source venv/bin/activate
 python manage.py runserver
