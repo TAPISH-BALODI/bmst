7c4dbd23e7b8a649a7b164b72f3227291a4d8629
TapishBalodi <tapishbalodi7@gmail.com>
2024-03-24 17:44:34 +0530

    push

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
-python -m venv venv
-source venv/Scripts/activate.bat
+@echo off
+
+
+call venv/Scripts/activate.bat
+
 
 pip install -r requirements.txt
 
-# Run migrations (if applicable)
+
 python manage.py migrate
 
-# Collect static files (if applicable)
+
 python manage.py collectstatic --noinput
 
-python manage.py runserver
-# Deactivate the virtual environment
-deactivate
+
