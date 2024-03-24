0eccedab31b2e015b0e0638495771ab1c104e275
TapishBalodi <tapishbalodi7@gmail.com>
2024-03-24 12:54:19 +0530

    push

--- build.sh
+++ build.sh
@@ -6,5 +6,8 @@ pip install -r requirements.txt
 # Run migrations (if applicable)
 python manage.py migrate
 
+# Collect static files (if applicable)
+python manage.py collectstatic --noinput
+
 # Deactivate the virtual environment
 deactivate
