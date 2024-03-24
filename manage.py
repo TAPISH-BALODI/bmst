88217fdb963aa3bca8b963db87dc85af41f9e751
TapishBalodi <tapishbalodi7@gmail.com>
2024-03-24 13:55:07 +0530

    push

--- manage.py
+++ manage.py
@@ -6,6 +6,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    # Add the path to your Django project
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(os.path.join(current_path, 'bmsBackend'))
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bmsBackend.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -14,7 +18,7 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from exc
+        ) 
     execute_from_command_line(sys.argv)
 
 
